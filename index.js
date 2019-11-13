46a16cedd8a2b58bbc5bd06fb5ce22f609eb634b
Glitch (typhoon-exception) <none>
2019-11-13 23:06:21 +0000

    🍠🐪 Checkpoint ./warns.json:8551325/2 ./index.js:8551325/636

--- index.js
+++ index.js
@@ -114,7 +114,15 @@ client.on("message", message => {
     message.channel.send(
       member + " a été warn pour " + reason + " :white_check_mark:"
     );
-  }
+  };
+  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+        let member = message.mentions.members.first()
+        if (!member) return message.channel.send("Veuillez mentionner un membre")
+        let embed = new Discord.RichEmbed()
+            .setAuthor(member.user.username, member.user.displayAvatarURL)
+            .addField('10 derniers warns', ((warns[member.id] && warns[member.id].length) ? warns[member.id].slice(0, 10).map(e => e.reason) : "Ce membre n'a aucun warns"))
+            .setTimestamp()
+        message.channel.send(embed)
 });
 
 client.on("guildMemberAdd", user => {
