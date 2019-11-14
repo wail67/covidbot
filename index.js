2161eafa4878e814ea77aefc8c020c1d15711b2e
Glitch (typhoon-exception) <none>
2019-11-14 22:59:01 +0000

    🌞🍨 Checkpoint ./index.js:8551325/871 ./warns.json:8551325/457

--- index.js
+++ index.js
@@ -177,8 +177,19 @@ client.on("message", message => {
                 message.channel.send(member + ' a été mute :white_check_mark:')
             })
         }
+}
 
-  }});
+  if (message.content.startsWith("!unmute")){  
+  if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande.")
+        let member = message.mentions.members.first()
+        if(!member) return message.channel.send("Membre introuvable")
+        if(member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas unmute ce membre.")
+        if(!member.manageable) return message.channel.send("Je ne pas unmute ce membre.")
+        let muterole = message.guild.roles.find(role => role.name === 'Muted')
+        if(muterole && member.roles.has(muterole.id)) member.removeRole(muterole)
+        message.channel.send(member + ' a été unmute :white_check_mark:')
+  }
+});
 
 
 
