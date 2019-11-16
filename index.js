47c1c4d99e0f9d9e76a44d0066c223b94b3c6729
Glitch (botredblood) <none>
2019-11-16 12:26:20 +0000

    👜🛩 Checkpoint ./index.js:8551325/2062 ./Events/message.js:8551325/432 ./Events/ready.js:8551325/36

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ client.on("guildMemberAdd", user => {
     );
 });
 
+
+
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
+
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
+});
+
 fs.readdirSync('./Commandes/', (error, f) => {
     if (error) { return console.error(error); }
         let commandes = f.filter(f => f.split('.').pop() === 'js');
@@ -29,6 +42,6 @@ fs.readdirSync('./Commandes/', (error, f) => {
             console.log(`${f} commande chargée !`);
             client.commands.set(commande.help.name, commande);
         });
-})
+});
 
 client.login(process.env.TOKEN);
