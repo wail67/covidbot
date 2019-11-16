2979d74e20d1d1b316616fc795c2494b01c9ed0b
Glitch (botredblood) <none>
2019-11-16 09:44:53 +0000

    🚩💳 Checkpoint ./index.js:8551325/2382

--- index.js
+++ index.js
@@ -176,7 +176,32 @@ client.on("message", message => {
       return message.channel.send(
         "Vous n'avez pas la permission d'utiliser cette commande"
       );
-    message.channel.bulkDelete();
+  async function purge() {
+            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
+
+            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
+            if (!message.member.roles.find("name", "BOSS")) { // This checks to see if they DONT have it, the "!" inverts the true/false
+                message.channel.send('You need the \`BOSS\` role to use this command.'); // This tells the user in chat that they need the role.
+                return; // this returns the code, so the rest doesn't run.
+            }
+
+            // We want to check if the argument is a number
+            if (isNaN(args[1])) {
+                // Sends a message to the channel.
+                message.channel.send('Please use a number as your arguments. \n Usage:  ! purge <amount>'); //\n means new line.
+                // Cancels out of the script, so the rest doesn't run.
+                return;
+            }
+
+            const fetched = await message.channel.fetchMessages({limit: args[1]}); // This grabs the last number(args) of messages in the channel.
+            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
+
+            // Deleting the messages
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
+
+        }
+  purge();
   }
 
   if (message.content.startsWith("!mute")) {
