64903965133c04241975ea6646bdc89c998020db
Glitch (botredblood) <none>
2019-11-18 22:45:29 +0000

    ⛈🐉 Checkpoint ./Commandes/unban.js:8551325/4837 ./index.js:8551325/42

--- Commandes/unban.js
+++ Commandes/unban.js
@@ -1,58 +1,19 @@
 const Discord = require('discord.js');
 const fs = require("fs");
+const superagent = require("superagent")
 
 module.exports.run = (client, message, args, warns) => {
-    if (!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) {
-      let member = message.guild.member(message.author);
-      let reason = "Utilisation de commande inaproprié";
-      if (!warns[member.id]) {
-        warns[member.id] = [];
-      }
-      warns[member.id].unshift({
-        reason: reason,
-        date: Date.now()
-      });
-      fs.writeFileSync("./warns.json", JSON.stringify(warns));
-      message.channel.send("Vous ne pouvez pas warn ce membre");
-
-      return message.channel.bulkDelete(2, true);
-    }
-
-    const user = message.mentions.users.first();
-    if (user) {
-      const member = message.guild.member(user);
-      if (member) {
-        let reason = args[2].split('&nbsp').join(' ')
-        if(!reason) return
-        let day = args[3]
-
-        message.channel.send(day)
-        member.guild.ban(member, {days: day, reason: reason})
-          .then(() => {
-      let embed = new Discord.RichEmbed()
-      .setColor('#ed2c13')
-      .setAuthor("Mon Bot")
-      .addField("Ban par:",""+ message.author.username)
-      .addField("Pseudo", + member)
-      .addField("Id : ",""+ member.guild.id)
-      .addField("Raison : ",""+ reason)
-      .addField("Pour : ",""+ day+ " jours")
-      .setTimestamp();
-    message.channel.send(embed);
-          })
-          .catch(err => {
-            message.reply("Je n'ai pas la permission de ban cette personne");
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("Cette personne n'est pas présente sur le serveur");
-      }
-    } else {
-      message.reply("Tu n'a pas mentionné la personne à ban !");
-    }
+     let reason = args.slice(1).join(' ');
+  client.unbanReason = reason;
+  client.unbanAuth = message.author;
+  let user = args[0];
+  let modlog = client.channels.find('name', 'commande-admin');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
+  if (!user) return message.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
+  message.guild.unban(user);
 };
 
 module.exports.help = {
-    name: 'ban'
+    name: 'unban'
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const fs = require("fs");
-
+const superagent = require("superagent")
 
 
 
