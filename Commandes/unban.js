be655de1a39ca7f8cbbe1ed71149b2ee34d105c9
Glitch (botredblood) <none>
2019-11-18 23:03:58 +0000

    💍🌿 Checkpoint ./Commandes/unban.js:8551325/182

--- Commandes/unban.js
+++ Commandes/unban.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 const superagent = require("superagent")
 
 module.exports.run = (client, message, args, warns) => {
-     let reason = args.slice(1).join(' ');
+  let reason = args.slice(1).join(' ');
   client.unbanReason = reason;
   client.unbanAuth = message.author;
-  let user = args[0];
+  let user = args[1];
   let modlog = client.channels.find('name', 'commande-admin');
   if (!modlog) return message.reply('I cannot find a mod-log channel');
   if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
-  if (!user) return message.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
-  message.guild.unban(user);
+  if (!user) return message.reply('You must supply a User Resolvable, such as a user id.')
+  message.guild.unban(user, {reason : reason});
 };
 
 module.exports.help = {
