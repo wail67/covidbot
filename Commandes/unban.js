f6b93db6dc42a0b25293cec173749704c9235c94
Glitch (botredblood) <none>
2019-11-18 23:15:56 +0000

    🏘🍱 Checkpoint ./Commandes/unban.js:8551325/2057 ./coulours.json:8551325/447

--- Commandes/unban.js
+++ Commandes/unban.js
@@ -2,16 +2,36 @@ const Discord = require('discord.js');
 const fs = require("fs");
 const superagent = require("superagent")
 
-module.exports.run = (client, message, args, warns) => {
-  let reason = args.slice(1).join(' ');
-  client.unbanReason = reason;
-  client.unbanAuth = message.author;
-  let user = args[1];
-  let modlog = client.channels.find('name', 'commande-admin');
-  if (!modlog) return message.reply('I cannot find a mod-log channel');
-  if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
-  if (!user) return message.reply('You must supply a User Resolvable, such as a user id.')
-  message.guild.unban(user, {reason : reason});
+module.exports.run = async (client, message, args, warns) => {
+  if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You dont have permission to perform this command!")
+
+    let bannedMember = await client.fetchUser(args[1])
+        if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+
+    let reason = args.slice(2).join(" ")
+        if(!reason) reason = "No reason given!"
+    message.channel.send(args)
+    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command!")|
+    message.delete()
+    try {
+        message.guild.unban(bannedMember, {reason: reason})
+        message.channel.send(`${bannedMember.tag} has been unbanned from the guild!`)
+    } catch(e) {
+        console.log(e.message)
+    }
+
+    let embed = new Discord.RichEmbed()
+    .setColor("#f94343")
+    .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+    .addField("Moderation:", "unban")
+    .addField("Moderated on:", `${bannedMember.username} (${bannedMember.id})`)
+    .addField("Moderator:", message.author.username)
+    .addField("Reason:", reason)
+    .addField("Date:", message.createdAt.toLocaleString())
+    
+        let sChannel = message.guild.channels.find(c => c.name === "commande-admin")
+        sChannel.send(embed)
+
 };
 
 module.exports.help = {
