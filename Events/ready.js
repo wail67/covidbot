4ea2a43f47413a30a9b81b83c106fec08daf05a9
Glitch (botredblood) <none>
2019-11-21 21:57:38 +0000

    📡🍚 Checkpoint ./Events/ready.js:8551325/45

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     client.user.setPresence({
         game: {
-            name: "Baise tes mort"
+            name: "!help et je viens à ton secours"
         }
     });
 };
